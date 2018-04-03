8f052ccd56d482603dfa37d4ffbc04119641b48a
b6027534 <b6027534@my.shu.ac.uk>
2018-04-03 22:10:26 +0100

    Sucessful unit test for Shift.groovy

--- hrSystem/grails-app/domain/com/mlaughton/Shift.groovy
+++ hrSystem/grails-app/domain/com/mlaughton/Shift.groovy
@@ -6,6 +6,33 @@ String dayOfWeek
 int numberOfHours
 String startingTime
 
+boolean theTimeEqualsStartingTime() {
+if (timeOfDay == 'Morning') {
+	if (startingTime == '1:00am' || startingTime == '2:00am' || startingTime == '3:00am' 
+|| startingTime == '4:00am' || startingTime == '5:00am' || startingTime == '6:00am' || startingTime == '7:00am' 
+|| startingTime == '8:00am' || startingTime == '9:00am' || startingTime == '10:00am' || startingTime == '11:00am') {
+return true
+	}
+}
+
+
+if (timeOfDay == 'Afternoon') {
+	if (startingTime == '12:00am' || startingTime == '13:00am' || startingTime == '14:00am' 
+|| startingTime == '15:00am' || startingTime == '16:00am' || startingTime == '17:00am') {
+return true
+	}
+}
+
+
+if (timeOfDay == 'Evening') {
+	if (startingTime == '18:00am' || startingTime == '19:00am' || startingTime == '20:00am' 
+|| startingTime == '21:00am' || startingTime == '22:00am' || startingTime == '23:00am' || startingTime == '12:00pm') {
+return true
+	}
+}
+return false
+}
+
     static constraints = {
 timeOfDay nullable: false, blank: false
 dayOfWeek nullable: false, blank: false
--- hrSystem/src/test/groovy/com/mlaughton/ShiftSpec.groovy
+++ hrSystem/src/test/groovy/com/mlaughton/ShiftSpec.groovy
@@ -11,8 +11,13 @@ class ShiftSpec extends Specification implements DomainUnitTest<Shift> {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void timeEqualsStartingTime() {
+        when: "the timeOfDay is equvilent to the starting time (e.g. 6:00am = morning)"
+
+def shift=new Shift(timeOfDay:'Morning', startingTime:'6:00am')
+
+then: "the return value will be true"
+
+shift.theTimeEqualsStartingTime()==true
     }
 }
