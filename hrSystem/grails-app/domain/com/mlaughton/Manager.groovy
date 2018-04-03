272a4640004b74035846e7cf656da124ae192cb3
b6027534 <b6027534@my.shu.ac.uk>
2018-04-03 18:03:48 +0100

    Controller clases created for each domain

--- hrSystem/grails-app/domain/com/mlaughton/Manager.groovy
+++ hrSystem/grails-app/domain/com/mlaughton/Manager.groovy
@@ -2,6 +2,19 @@ package com.mlaughton
 
 class Manager {
 
+String fullName
+String userName
+String password
+String managerEmail
+String office
+String department
+
     static constraints = {
+fullName nullable: false, blank: false
+userName nullable: false, blank: false, unique: true
+password nullable: false, blank: false
+managerEmail nullable: false, blank: false, email: true
+office nullable: false, blank, false
+department nullable: false, blank: false
     }
 }
